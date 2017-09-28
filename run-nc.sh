b4706dc353655e5ba8faf78da96a247c4f1e56ca
Christopher LILJENSTOLPE <cdl@asgaard.org>
2017-09-28 05:59:56 -0700

    added file to echo out

--- run-nc.sh
+++ run-nc.sh
@@ -9,7 +9,7 @@ set -o nounset
 if [ $? -eq 0 ] ; then 
   echo -n "${STACK_CLUSTER}:  " > /tmp/ack
   uuidgen | cut -c 32- >> /tmp/ack
-  echo "cmd: /usr/bin/nc ${NC_CMD_ARGS}" 
+  echo "cmd: /usr/bin/nc ${NC_CMD_ARGS} < /tmp/ack" 
   while true
   do 
     ifconfig -a | grep inet | /bin/nc ${NC_CMD_ARGS}
